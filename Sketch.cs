10f1fddb753cb68d0dc467ce863fdc3eb332bf5d
Christopher Hayes <chrisdhjx@gmail.com>
2017-08-28 20:09:14 +1200

    Add files via upload

--- Sketch.cs
+++ Sketch.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class Sketch : MonoBehaviour {
 
     public GameObject myPrefab;
-    public randomcolor randomcol;
+    public Color lerpedColor = Color.white;
+    private float nextTime = 0.0f;
+    public float period = 1.0f;
+    public List<GameObject> cubey;
+
 
 	// Use this for initialization
 	void Start () {
 
         int totalCubes = 18;
         float totalDistance = 2.5f;
+
+        cubey = new List<GameObject>();
 
         for (int i = 0; i < totalCubes; i++) 
         {
@@ -27,7 +33,7 @@ public class Sketch : MonoBehaviour {
             newCube.GetComponent<Mycudescript>().SetSize(.5f * (1.0f - perc) );
             newCube.GetComponent<Mycudescript>().rotateSpeed = .2f + perc*3.0f;
 
-            var changecolor = newCube.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+            cubey.Add(newCube);
         }
 
         
@@ -42,7 +48,16 @@ public class Sketch : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Time.time > nextTime)
+        {
+            nextTime += period;
+            foreach (var cube in cubey)
+            {
+                Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+                cube.GetComponent<Renderer>().material.color = newColor;
+            }
 
+        }
      
 	}
 }
